3f565af37296a055c847b1746a2b2b37a3cef355
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-09 22:19:22 +0100

    update

--- libps/ft_lis.c
+++ libps/ft_lis.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 23:37:52 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/09 16:43:06 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/09 22:18:29 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,14 @@ int	*ft_rev_lis(int *a, int len_a, int index, int f())
 	i_and_lis = ft_malloc(3 * 4);
 	i_and_lis[0] = len_a;
 	i_and_lis[1] = 0;
-	lis = 1;
 	tmp = *a;
 	while (index > len_a / 2)
 	{
 		i = index;
+		lis = 1;
 		while (--i >= 0)
-			tmp = f(*a, i, tmp, &lis);
-		if ((i - lis) < (i_and_lis[2]) - (i_and_lis[1]))
+			tmp = f(a, i, tmp, &lis);
+		if ((i - lis) < (i_and_lis[2] - i_and_lis[1]))
 		{
 			i_and_lis[0] = index;
 			i_and_lis[1] = lis;
@@ -62,16 +62,17 @@ int	*ft_lis(int *a, int len_a, int index, int f())
 	int	*i_and_lis;
 
 	i_and_lis = ft_malloc(3 * 4);
-	i_and_lis[0] = len_a;
+	i_and_lis[2] = len_a;
 	i_and_lis[1] = 0;
-	len = 1;
 	tmp = *a;
 	while (index < len_a / 2)
 	{
 		i = index;
+		len = 1;
 		while (++i < len_a)
-			tmp = f(*a, i, tmp, &len);
-		if ((i - len) < (i_and_lis[2]) - (i_and_lis[1]))
+			tmp = f(a, i, tmp, &len);
+		printf("i - len: %d	i_and_lis[2] - i_and_lis[1]: %d\n", i - len, i_and_lis[2] - i_and_lis[1]);
+		if ((i - len) < (i_and_lis[2] - i_and_lis[1]))
 		{
 			i_and_lis[0] = index;
 			i_and_lis[1] = len;
@@ -82,16 +83,16 @@ int	*ft_lis(int *a, int len_a, int index, int f())
 	return (i_and_lis);
 }
 
-int	*ft_find_lis(int *a, int len_a)
-{
-	int	*la_i_and_lis;
-	int	*ld_i_and_lis;
-	int	*rla_i_and_lis;
-	int	*rld_i_and_lis;
+// int	*ft_find_lis(int *a, int len_a)
+// {
+// 	int	*la_i_and_lis;
+// 	int	*ld_i_and_lis;
+// 	int	*rla_i_and_lis;
+// 	int	*rld_i_and_lis;
 
-	la_i_and_lis = ft_lis(a, len_a, 0, ascending);
-	ld_i_and_lis = ft_lis(a, len_a, 0, descending);
-	rla_i_and_lis = ft_rev_lis(a, len_a, len_a, ascending);
-	rld_i_and_lis = ft_rev_lis(a, len_a, len_a, descending);
+// 	la_i_and_lis = ft_lis(a, len_a, 0, ascending);
+// 	ld_i_and_lis = ft_lis(a, len_a, 0, descending);
+// 	rla_i_and_lis = ft_rev_lis(a, len_a, len_a, ascending);
+// 	rld_i_and_lis = ft_rev_lis(a, len_a, len_a, descending);
 	
-}
+// }
