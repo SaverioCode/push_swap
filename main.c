c0a3750e5081b72da13e7b150392ad62bfc34abd
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-14 00:27:46 +0100

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 01:19:01 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/14 00:24:10 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/14 00:27:33 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ void	push_swap(int *a, int *b, int *len_a, int *len_b)
 	int	*i1_i2;  // numero da inserire, indice del numero da inserire, indice del numero nell'altro stack
 	
 	lis_len = 0;
-	lis = ft_lis(a, len_a, &lis_len);
-	while (len_a > lis_len)
+	lis = ft_lis(a, *len_a, &lis_len);
+	while (*len_a > lis_len)
 	{
-		i1_i2 = ft_fstmv(*a, *b, len_a, len_b);
+		i1_i2 = ft_fstmv(a, b, *len_a, *len_b);
 		while (i1_i2[0] > 0 && i1_i2[1] > 0 && --i1_i2[0] && --i1_i2[1])
-			ft_rr(a, b, len_a, len_b);
+			ft_rr(a, b, *len_a, *len_b);
 		while (i1_i2[0] > 0 && --i1_i2[0])
-			ft_ra(a, len_a, 1);
+			ft_ra(a, *len_a, 1);
 		while (i1_i2[1] > 0 && --i1_i2[1])
-			ft_rb(b, len_b, 1);
-		ft_pb(&a, b, &len_a, &len_b);
+			ft_rb(b, *len_b, 1);
+		ft_pb(&a, b, len_a, len_b);
 	}
 	free(i1_i2);
 	free(lis);
@@ -49,7 +49,7 @@ int	main(int ac, char **av)
 	b = ft_malloc(1);
 	ft_astai(a, av[1]);
 	// do the magic //
-	// push_swap(&a, &b, &len_a, &len_b);
+	push_swap(a, b, &len_a, &len_b);   /// still in testing
 	write(1, "B\n", 2); /////////
 	// magic ended //
 	/// testing ///
