efb0f928fdc23a5326f3771c63b2fb50b0003c86
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-03-17 03:35:26 +0100

    update

--- libps/ft_fstmv.c
+++ libps/ft_fstmv.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 00:12:00 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/03/17 03:10:24 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/03/17 03:33:36 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	fstmv_end_start(t_stack *s, int i)
 		if (is_suitable_0_rev(s->a, s->b, s->len_b, i))
 			updt_fstmv(i, s->len_b - 1, s, 0);
 		j = s->len_b - 1;
-		while (--j > (s->len_b / 2) - 1)
+		while (--j >= 0)
 			if (is_suitable_rev(s->a, s->b, i, j))
 				updt_fstmv(i, j, s, 0);
 	}
@@ -93,7 +93,7 @@ void	ft_fstmv(t_stack *s)
 	{
 		while (i >= 0 && ft_islis(s, s->a[i]))
 			i--;
-		if (i >= 0)
+		if (i < 0)
 			break;
 		fstmv_end_start(s, i);
 	}
